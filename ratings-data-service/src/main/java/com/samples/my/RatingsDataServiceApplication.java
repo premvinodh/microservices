c74a89ca31447a4bff8524491b301b137cc58878
premvinodh <premvinodh.pc@gmail.com>
2021-11-21 17:12:23 +0530

    Adding ratings-data-service

---
+++ ratings-data-service/src/main/java/com/samples/my/RatingsDataServiceApplication.java
@@ -0,0 +1,14 @@
+package com.samples.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+}
---
+++ ratings-data-service/src/main/java/com/samples/my/resources/RatingsDataResource.java
@@ -0,0 +1,30 @@
+package com.samples.my.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.samples.my.models.Rating;
+import com.samples.my.models.UserRating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsDataResource {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+		return new Rating(movieId, "4");
+	}
+
+	@RequestMapping("users/{userId}")
+	public UserRating getUserRating(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(new Rating("1234", "4"), new Rating("4567", "3"));
+
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratings);
+		return userRating;
+	}
+}
